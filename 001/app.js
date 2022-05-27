d2f5e3a2d969936c0c8fb9e5a7a985af558c2a7d
sushi200 <pawarsushant531@gmail.com>
2022-05-27 22:52:20 +0530

    basics added

--- 001/app.js
+++ 001/app.js
@@ -1,8 +1,23 @@
+const keys = Array.from(document.querySelectorAll('.key'))
+keys.forEach(key => key.addEventListener('transitioned', removeTransition))
+window.addEventListener('keydown',playSound)
 
- window.addEventListener('keydown', function (e) {
-            const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-            console.log(audio)
-            if (!audio) return;// stop the function from returning
-            audio.currentTime = 0;
-            audio.play();
-        });
+//function playSound
+function playSound(e){
+    const audio = document.querySelector('.key')
+    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`)
+    
+    //if statement
+    //buggy
+    if(!audio) return;
+
+    key.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+}
+
+//removing transition  //buggy
+function removeTransition(e){
+    if(e.property !== 'transform') return
+    e.target.classList.remove('playing')
+}
